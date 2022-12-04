43927f25aa66c7f544ea79fd597512898a373b2b
Eman Harout <emanharout@gmail.com>
2022-12-03 21:53:17 -0800

    Add nunjucks + extract header (logo+nav) to a partial

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,20 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./source/styles/css/");
   eleventyConfig.addWatchTarget("./source/styles/css/");
+  eleventyConfig.setNunjucksEnvironmentOptions({
+    throwOnUndefined: true,
+    autoescape: false,
+  });
 
   return {
     dir: {
       input: "source",
+      includes: "_includes",
       output: "public"
-    }
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
   };
 };
